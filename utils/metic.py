f25c5269578d853e84c20acf4a80172ae343fe06
Ah-Yeong Jeong <113572871+ayjxxng@users.noreply.github.com>
2024-06-22 13:59:43 +0900

    Update metic.py

--- utils/metic.py
+++ utils/metic.py
@@ -7,20 +7,20 @@ from sklearn.metrics import confusion_matrix, roc_auc_score, accuracy_score
 def get_pred(predictions):
     stack = torch.from_numpy(np.vstack(predictions))
     _, preds = torch.max(F.softmax(stack, -1).data, -1) 
-    proba = F.softmax(stack, -1).data[:, 1:].squeeze(-1) 
+    proba = F.softmax(stack, -1).data[:, 1:].squeeze(-1)
 
     return preds, proba
 
 
 def confusion(g_true, predictions):
-    pred, prob = get_pred(predictions) 
+    pred, prob = get_pred(predictions)
     g_true = torch.from_numpy(np.vstack(g_true))
-    _, lab = torch.max(g_true, -1) 
+    _, lab = torch.max(g_true, -1)
     acc = accuracy_score(lab, pred)
 
-    tn, fp, fn, tp = confusion_matrix(lab,pred).ravel()
-    sens = (tp)/(tp+fn)
-    spec = (tn)/(tn+fp)
+    tn, fp, fn, tp = confusion_matrix(lab, pred).ravel()
+    sens = tp / (tp + fn)
+    spec = tn / (tn + fp)
     auc = roc_auc_score(lab, prob)
 
     precision = tp / (tp + fp)
