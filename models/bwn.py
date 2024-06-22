68f0dc289994b2724c1ecef59620b910205e7d44
Ah-Yeong Jeong <113572871+ayjxxng@users.noreply.github.com>
2024-06-22 15:35:39 +0900

    Update bwn.py

--- models/bwn.py
+++ models/bwn.py
@@ -15,7 +15,7 @@ class TS_Encoder(nn.Module):
         self.N = cfg.dataset.n_channels + 1  # 1 for SCT
         self.T = cfg.dataset.n_times
 
-        # Linear Layer
+        # Linear Layers
         self.D_to_T = nn.Linear(self.D, self.T, dtype=cfloat)
         self.T_to_D = nn.Linear(self.T, self.D, dtype=cfloat)
 
@@ -26,25 +26,26 @@ class TS_Encoder(nn.Module):
         self.spatial_blocks = SpatialEnc(cfg)
 
     def forward(
-            self,
-            temp_emb: torch.Tensor,
-            spat_emb: torch.Tensor
+            self, temp_emb: torch.Tensor, spat_emb: torch.Tensor
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         """
-        Inputs:
-            temp_emb: temporal embedding input [B, N+1, F+1, T]
-            spat_emb: spatial embedding input [B, N+1, 1, D]
+        Args:
+            temp_emb: Temporal embedding input of shape [B, N+1, F+1, T]
+            spat_emb: Spatial embedding input of shape [B, N+1, 1, D]
 
-        Outputs:
-            temp_emb: temporal embedding output [B, (N+1), F+1, T]
-            spat_emb: spatial embedding output [B, (N+1), 1, D]
+        Returns:
+            temp_emb: Temporal embedding output of shape [B, (N+1), F+1, T]
+            spat_emb: Spatial embedding output of shape [B, (N+1), 1, D]
         """
         # Element-wise addition between SE and FCT
         temp_emb += nn.functional.pad(self.D_to_T(spat_emb), (0, 0, 0, self.F)).to(device)
+
         # TemporalEnc
         temp_emb = self.temporal_blocks(temp_emb)
+
         # FCT slicing (first raw) + back to D
         spat_emb = spat_emb + self.T_to_D(temp_emb[:, :, :1, :])
+
         # SpatialEnc
         spat_emb = self.spatial_blocks(spat_emb)
 
@@ -78,18 +79,16 @@ class WaveletTF(nn.Module):
         ])
 
     def forward(
-            self,
-            x_real: torch.Tensor,
-            x_imag: torch.Tensor
+            self, x_real: torch.Tensor, x_imag: torch.Tensor
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         """
-        Input:
-            x_real: [B, N, F, T]
-            x_imag: [B, N, F, T]
+        Args:
+            x_real: Real part of the input tensor of shape [B, N, F, T]
+            x_imag: Imaginary part of the input tensor of shape [B, N, F, T]
 
-        Output:
-            temp_emb: [B, (N+1), 1, D]
-            spec_emb: [B, (N+1), 1, D]
+        Returns:
+            temp_emb: Temporal embedding output of shape [B, (N+1), F+1, T]
+            spat_emb: Spatial embedding output of shape [B, (N+1), 1, D]
         """
         batch_size = len(x_real)
         x = torch.complex(x_real, x_imag).to(device)
@@ -98,9 +97,8 @@ class WaveletTF(nn.Module):
         fct = torch.repeat_interleave(self.fct, batch_size, 0).to(device)
         temp_emb = torch.cat([fct, x], dim=2)
         temp_emb = temp_emb + self.fpe
-        temp_emb = nn.functional.pad(
-            temp_emb, (0, 0, 0, 0, 1, 0)
-        )
+        temp_emb = nn.functional.pad(temp_emb, (0, 0, 0, 0, 1, 0))
+
         # Initialize spatial embedding
         spat_emb = torch.repeat_interleave(self.se, batch_size, 0).to(device)
         sct = torch.repeat_interleave(self.sct, batch_size, 0).to(device)
@@ -122,7 +120,7 @@ class BrainWaveNet(nn.Module):
         self.T = cfg.dataset.n_times
         self.n_classes = cfg.dataset.n_classes
 
-        # Residual Conv Block
+        # Residual Conv Blocks
         self.fe_model_real = Res2DModule(cfg)
         self.fe_model_imag = Res2DModule(cfg)
 
@@ -134,16 +132,16 @@ class BrainWaveNet(nn.Module):
 
     def forward(self, features: torch.Tensor) -> torch.Tensor:
         """
-        Input:
-            features: [B, T, F, N, 2] (Real&Imag)
+        Args:
+            features: Input tensor of shape [B, T, F, N, 2] (Real & Imag)
 
-        Output:
-            logits: [B, n_classes]
+        Returns:
+            out: Logits of shape [B, n_classes]
         """
         features_real = features[:, :, :, :, 0].permute(0, 3, 2, 1)
         features_imag = features[:, :, :, :, 1].permute(0, 3, 2, 1)
 
-        # Residual Conv Block
+        # Residual Conv Blocks
         fe_out_real = self.fe_model_real(features_real)
         fe_out_imag = self.fe_model_imag(features_imag)
 
@@ -152,6 +150,4 @@ class BrainWaveNet(nn.Module):
         out = torch.stack([spat_emb.real[:, 0, 0, :], spat_emb.imag[:, 0, 0, :]], dim=-1)
         out = self.linear_out(out.reshape(out.size(0), -1))
 
-        return out
-        
         return out
