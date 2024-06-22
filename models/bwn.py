1ca1ca164a05c5c1edd16d5a42f2ec8ede0b6658
Ah-Yeong Jeong <113572871+ayjxxng@users.noreply.github.com>
2024-06-22 15:02:09 +0900

    Update bwn.py

--- models/bwn.py
+++ models/bwn.py
@@ -8,10 +8,7 @@ device = torch.device("cuda")
 
 
 class TS_Encoder(nn.Module):
-    def __init__(
-            self,
-            cfg: DictConfig
-    ) -> None:
+    def __init__(self, cfg: DictConfig) -> None:
         super().__init__()
         self.D = cfg.model.embed_dim
         self.F = cfg.dataset.n_frequencies
@@ -42,16 +39,12 @@ class TS_Encoder(nn.Module):
             temp_emb: temporal embedding output [B, (N+1), F+1, T]
             spat_emb: spatial embedding output [B, (N+1), 1, D]
         """
-
         # Element-wise addition between SE and FCT
-        temp_emb = temp_emb + \
-                   nn.functional.pad(self.D_to_T(spat_emb), (0, 0, 0, self.F)).to(device)
-
+        temp_emb += nn.functional.pad(self.D_to_T(spat_emb), (0, 0, 0, self.F)).to(device)
+        # TemporalEnc
         temp_emb = self.temporal_blocks(temp_emb)
-
         # FCT slicing (first raw) + back to D
         spat_emb = spat_emb + self.T_to_D(temp_emb[:, :, :1, :])
-
         # SpatialEnc
         spat_emb = self.spatial_blocks(spat_emb)
 
@@ -59,27 +52,24 @@ class TS_Encoder(nn.Module):
 
 
 class WaveletTF(nn.Module):
-    def __init__(
-            self,
-            cfg: DictConfig
-    ) -> None:
+    def __init__(self, cfg: DictConfig) -> None:
         super().__init__()
-        D = cfg.model.embed_dim
-        F = cfg.dataset.n_frequencies
-        N = cfg.dataset.n_channels
-        T = cfg.dataset.n_times
+        self.D = cfg.model.embed_dim
+        self.F = cfg.dataset.n_frequencies
+        self.N = cfg.dataset.n_channels
+        self.T = cfg.dataset.n_times
 
         # FCT: Frequency Class Token
-        self.fct = nn.Parameter(torch.zeros((1, N, 1, T), dtype=cfloat))
+        self.fct = nn.Parameter(torch.zeros((1, self.N, 1, self.T), dtype=cfloat))
 
         # Frequency Positional Encoding
-        self.fpe = nn.Parameter(torch.zeros((1, 1, F + 1, T), dtype=cfloat))
+        self.fpe = nn.Parameter(torch.zeros((1, 1, self.F + 1, self.T), dtype=cfloat))
 
         # SCT: Spatial Class Token
-        self.sct = nn.Parameter(torch.zeros((1, 1, 1, D), dtype=cfloat))
+        self.sct = nn.Parameter(torch.zeros((1, 1, 1, self.D), dtype=cfloat))
 
         # Spatial Embedding
-        self.se = nn.Parameter(torch.rand((1, N, 1, D), dtype=cfloat))
+        self.se = nn.Parameter(torch.rand((1, self.N, 1, self.D), dtype=cfloat))
 
         # WaveletTF blocks
         self.waveletTF_block = nn.ModuleList([
@@ -109,8 +99,8 @@ class WaveletTF(nn.Module):
         temp_emb = torch.cat([fct, x], dim=2)
         temp_emb = temp_emb + self.fpe
         temp_emb = nn.functional.pad(
-            temp_emb, (0, 0, 0, 0, 1, 0))
-
+            temp_emb, (0, 0, 0, 0, 1, 0)
+        )
         # Initialize spatial embedding
         spat_emb = torch.repeat_interleave(self.se, batch_size, 0).to(device)
         sct = torch.repeat_interleave(self.sct, batch_size, 0).to(device)
@@ -119,14 +109,12 @@ class WaveletTF(nn.Module):
         # WaveletTF blocks inference
         for block in self.waveletTF_block:
             temp_emb, spat_emb = block(temp_emb, spat_emb)
+
         return temp_emb, spat_emb
 
 
 class BrainWaveNet(nn.Module):
-    def __init__(
-            self,
-            cfg: DictConfig
-    ) -> None:
+    def __init__(self, cfg: DictConfig) -> None:
         super().__init__()
         self.D = cfg.model.embed_dim
         self.F = cfg.dataset.n_frequencies
@@ -146,7 +134,7 @@ class BrainWaveNet(nn.Module):
 
     def forward(
             self,
-            features: torch.Tensor,
+            features: torch.Tensor
     ) -> torch.Tensor:
         """
         Input:
@@ -155,16 +143,16 @@ class BrainWaveNet(nn.Module):
         Output:
             logits: [B, n_classes]
         """
-        features_real = features[:, :, :, :, 0]
-        features_imag = features[:, :, :, :, 1]
+        features_real = features[:, :, :, :, 0].permute(0, 3, 2, 1)
+        features_imag = features[:, :, :, :, 1].permute(0, 3, 2, 1)
+
         # Residual Conv Block
-        features_real = features_real.permute(0, 3, 2, 1)
         fe_out_real = self.fe_model_real(features_real)
-        features_imag = features_imag.permute(0, 3, 2, 1)
         fe_out_imag = self.fe_model_imag(features_imag)
 
         # Main model
         _, spat_emb = self.main_model(fe_out_real, fe_out_imag)
         out = torch.stack([spat_emb.real[:, 0, 0, :], spat_emb.imag[:, 0, 0, :]], dim=-1)
         out = self.linear_out(out.reshape(out.size(0), -1))
+        
         return out
